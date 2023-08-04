6194bcfb3427872b65d492c686fb37b6a620dafa
Tania Omelko <tanyaomelko39@gmail.com>
2023-08-04 18:26:04 +0300

    remove QueryClient

--- src/index.js
+++ src/index.js
@@ -4,15 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { QueryClientProvider } from 'react-query';
-import queryClient from './queryClient';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <App />
   </React.StrictMode>
 );
 
