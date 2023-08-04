439bc5b5e26ad52027dbbdcaf942c58a7ce6fa36
Tania Omelko <tanyaomelko39@gmail.com>
2023-08-04 19:43:58 +0300

    api js

--- src/api/api.js
+++ src/api/api.js
@@ -1,11 +1,22 @@
 import axios from "axios";
 
-// const URL = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json';
-const URL = 'https://api.jsonserve.com/p5uK3k';
+export const getData = async (cacheDuration = 3600000) => {
+  const cacheKey = 'fetchedData';
+  const url = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json';
+  const cachedData = JSON.parse(localStorage.getItem(cacheKey));
 
-export async function getData() {
-  const response = await axios.get(URL);
-  // console.log(response.data);
+  if (cachedData && Date.now() - cachedData.timestamp < cacheDuration) {
+    return cachedData.data;
+  }
 
-  return response.data;
-}
+  try {
+    const response = await axios.get(url);
+    const data = response.data;
+
+    localStorage.setItem(cacheKey, JSON.stringify({ data, timestamp: Date.now() }));
+
+    return data;
+  } catch (error) {
+    throw new Error('Error fetching data:', error);
+  }
+};
